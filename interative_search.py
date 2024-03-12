6feb603086f290e1cb9f2334990e648a2cf5bb44
Bernhard Retzl <retzl.bernhard@gmail.com>
2024-03-12 08:19:31 +0100

    gui added

--- interative_search.py
+++ interative_search.py
@@ -2,22 +2,32 @@ import subprocess
 import os
 from Bio import SeqIO
 import re
-import glob
+import PySimpleGUI as psg
 
 
 # https://bioinformatics.stackexchange.com/questions/7212/what-is-the-purpose-of-folder-locations-in-maxquant
+# https://www.tutorialspoint.com/pysimplegui/pysimplegui_popup_windows.htm
 
-def correct_mqpar_file(input_file, output_file, folder_number):
+
+def correct_mqpar_file(input_file, output_file, folder_number, fasta_file_name, path_to_ms_file, working_directory):
     with open(input_file) as f, open(output_file, 'w') as f_out:
         for line in f:
             if line.strip().startswith('<fastaFilePath>'):
                 start = line.split('<')[0]
-                f_out.write(start + '<fastaFilePath>' + fr'.\Data\Run\run_{folder_number}\uniprot_sprot.fasta' + '</fastaFilePath>\n')
+                f_out.write(start + '<fastaFilePath>' + fr'{working_directory}/Data/Run/run_{folder_number}/{fasta_file_name}' + '</fastaFilePath>\n')
             elif line.strip().startswith('<fixedCombinedFolder>'):
                 start = line.split('<')[0]
-                f_out.write(start + fr'<fixedCombinedFolder>.\Data\Run\run_{folder_number}</fixedCombinedFolder>'+'\n')
+                f_out.write(start + fr'<fixedCombinedFolder>{working_directory}/Data/Run/run_{folder_number}</fixedCombinedFolder>'+'\n')
+            elif line.strip().startswith('<filePaths>'):
+                f_out.write(line)
+                start = f.readline().split('<')[0]
+                f_out.write(start + '<string>' + path_to_ms_file + '</string>\n')
+            elif line.strip().startswith('<numThreads>'):
+                f_out.write(line)
+                num_threads = line.split('<numThreads>')[-1].split('</numThreads>')[0]
             else:
                 f_out.write(line)
+    return num_threads
 
 
 def write_new_fasta(protein_group_file, fasta_input_file, fasta_output_file):
@@ -36,23 +46,33 @@ def write_new_fasta(protein_group_file, fasta_input_file, fasta_output_file):
     out_handle.close()
 
 
-os.makedirs(f'./Data', exist_ok=True)
-os.makedirs(f'./Data/Run', exist_ok=True)
-os.makedirs(f'./Data/Run/run_1', exist_ok=True)
-os.makedirs(f'./Data/Start', exist_ok=True)
-
-print('Please place MS-file (.d), FASTA-file (.fasta) and the mqpar (mqpar.xml) in the folder ./Data/Start !')
-input('Press any key to continue...')
-
-correct_mqpar_file(f'./Data/Start/mqpar.xml','./Data/Run/run_1/mqpar.xml', folder_number=1)
-fasta_file = glob.glob('./Data/Start/*.fasta')[0]
-fasta_file = fasta_file.split(os.sep)[-1]
-os.replace(f'./Data/Start/' + fasta_file, './Data/Run/run_1/' + fasta_file)
+def make_folders():
+    os.makedirs(f'./Data', exist_ok=True)
+    os.makedirs(f'./Data/Run', exist_ok=True)
+    os.makedirs(f'./Data/Run/run_1', exist_ok=True)
+    os.makedirs(f'./Data/Start', exist_ok=True)
 
 
-path_to_MaxQuant = r'.\MaxQuant_v2.4.14.0\bin\MaxQuantCmd.exe'
+def get_paths_for_run():
+    path_to_MaxQuant = psg.popup_get_file(r'Please specify the path to MaxQuantCmd.exe (it´s in MaxQuant\bin\MaxQuantCmd.exe)',  title="File selector")
+    path_to_mqpar_file = psg.popup_get_file(r'Please specify the path to the mqpar.xml file',  title="File selector")
+    path_to_fasta_file = psg.popup_get_file(r'Please specify the path to the FASTA-file',  title="File selector")
+    path_to_ms_file = psg.popup_get_folder(r'Please specify the path to the MS (.d) file',  title="Folder selector")
+    fasta_file_name = path_to_fasta_file.split('/')[-1]
+    return path_to_MaxQuant, path_to_mqpar_file, path_to_fasta_file, path_to_ms_file, fasta_file_name
+
+
+make_folders()
+path_to_MaxQuant, path_to_mqpar_file, path_to_fasta_file, path_to_ms_file, fasta_file_name = get_paths_for_run()
+working_directory = os.getcwd().replace('\\', '/')
+
+
+threads_in_use = correct_mqpar_file(path_to_mqpar_file,'./Data/Run/run_1/mqpar.xml', folder_number=1, fasta_file_name=fasta_file_name, path_to_ms_file=path_to_ms_file, working_directory=working_directory)
+print('Attention ' + threads_in_use + ' threads in use!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+os.replace(path_to_fasta_file, './Data/Run/run_1/' + fasta_file_name)
+
+
 folder_number = 1
-
 for k in range(5):
     print('Run '+str(k))
     subprocess.call(path_to_MaxQuant + f' ./Data/Run/run_{folder_number}/mqpar.xml', shell=True)
@@ -61,4 +81,4 @@ for k in range(5):
     write_new_fasta(protein_group_file=f'./Data/Run/run_{folder_number-1}/combined/txt/proteinGroups.txt',
                     fasta_input_file=f'./Data/Run/run_{folder_number-1}/uniprot_sprot.fasta',
                     fasta_output_file=f'./Data/Run/run_{folder_number}/uniprot_sprot.fasta')
-    correct_mqpar_file(f'./Data/Run/run_{folder_number-1}/mqpar.xml',f'./Data/Run/run_{folder_number}/mqpar.xml', folder_number)
+    correct_mqpar_file(f'./Data/Run/run_{folder_number-1}/mqpar.xml', f'./Data/Run/run_{folder_number}/mqpar.xml', folder_number, fasta_file_name)
